eb687cdd6d83d7dcd4abe40576e427dfb49eeab7
lingfeng <lingfeng.li@going-link.com>
2022-06-07 15:08:09 +0800

    实现setTnterVal

--- index.js
+++ index.js
@@ -1,79 +1,43 @@
 
-1.// key:Array key代表事件名  Array事件列表  参考vue中 $emit $on
-2.  // 定义所有事件列表,此时需要修改格式：
-//   key: {
-//     id: Function,
-//     id: Function
-//   },
-//   key: Object,
-// } 
-3.   // 定义所有事件列表,此时需要修改格式：
-// // {
-//   key: {
-//     D+id: Function,
-//     id: Function
-//   },
-//   key: Object,
-// } 
+
+// 高阶函数 // todo 高阶函数可以把其他函数作为参数输入或者作为其返回值输出。
 
 
-/* 
-    实现一个简易的EventBus 在我们生活中 经常遇到微信自动弹出公众号消息 实质我们订阅了他,在有通知的的会推送回给我们
-    1. 订阅消息 发布消息 取消订阅 执行一次
-*/
-class EventBus {
-    constructor() {
-        this.EventBusObj = {}  // 事件集合
+// setTimeout 模拟 setInterval
 
-        this.callbackId = 0;  // 存储每个函数的id 用去取消订阅
+const mySetInterVal = (fn, time) => {
+    let timer = null;
+    const InterVal = () => {
+        timer = setTimeout(() => {
+            fn();
+            InterVal();
+        }, time);
     }
-    // 订阅事件
-    $on(name, callback) {
-        if (!this.EventBusObj[name]) {
-            this.EventBusObj[name] = {};
-        }
-        const id = this.callbackId++;
-        this.EventBusObj[name][id] = callback; // 以id形式存储回调函数
-        return id
-    }
-    // 发布事件
-    $emit(name, ...args) {
-        console.log(this.EventBusObj[name], 22)
-        const callbackList = this.EventBusObj[name]
-        for (const id in callbackList) {
-            callbackList[id](...args);
-        }
-    }
-    $cancel(name, id) {
-        delete this.EventBusObj[name][id];
-        console.log(`id为${id}的事件被取消了`)
-        if (!Object.keys(this.EventBusObj[name]).length) {
-            delete this.EventBusObj[name];
-        }
-    }
+    InterVal();
+    return () => clearTimeout(timer);
 }
 
 
-let EB = new EventBus();
+
+const cancel = mySetInterVal(() => console.log(3333), 2000)
+
+setTimeout(() => {
+    mySetInterVal(() => console.log(1), 1000)
+}, 1000)
 
 
-// 订阅事件
-EB.$on('key1', (name, age) => {
-    console.info("我是订阅事件A:", name, age);
-})
-let id = EB.$on("key1", (name, age) => {
-    console.info("我是订阅事件B:", name, age);
-})
-EB.$on("key2", (name) => {
-    console.info("我是订阅事件C:", name);
-})
+
+// setInterval 模拟 setTimeout 
+
+const mySetTimeOut = (fn, time) => {
+    let timer = null;
+    timer = setInterval(() => {
+        clearInterval(timer);
+        fn();
+    }, time)
+    return () => clearInterval(timer)
+}
 
 
-// 发布事件key1
-EB.$emit('key1', "小猪课堂", 26);
-// 取消订阅事件
-EB.$cancel('key1', id);
-// 发布事件key1
-EB.$emit('key1', "小猪课堂", 26);
-// 发布事件
-EB.$emit('key2', "小猪课堂");
+
+mySetTimeOut(() => console.log(2), 2000)
